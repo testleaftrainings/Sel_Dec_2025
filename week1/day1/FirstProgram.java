f8ea1cec1974ddb08e7b557f0ec8cc1ff06c1bb5
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-12-06 18:56:49 +0530

    Add files via upload

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	/**
+	 * shortcuts:
+	 * 1)main method: type main and do ctrl+space(entry point for the execution of the program)
+	 * 2)print statement:type syso and do ctrl+space
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello everyone welcome to testleaf for the selenium course");
+		System.out.println("happy learning");
+	}
+	
+}
---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+		byte noOfLearners=40;
+		short regNo=12345;
+		long phNo=8765434667L;
+		//to store decimal digits
+		float speed=0.15674892086f;
+		double speed1=0.15674892086;
+		System.out.println(speed);
+		System.out.println(speed1);
+		char logo='S';
+		char num='4';
+		//nonprimitive
+		String name="saranya";
+		String decBatch="there are around 60 learners";
+		System.out.println("learners in the dec batch:"+noOfLearners+"\n"+regNo+"\n"+logo+"\n"+decBatch);
+
+	}
+
+}
